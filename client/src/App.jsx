a2e555a53fcec874ce0dd112b9ab98bbadb0aca5
Julian Bowman <julianfbowman@gmail.com>
2021-04-14 17:18:59 -0600

    Fixed esLint errors

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,9 @@
+/* eslint-disable react/prefer-stateless-function */
 import React from 'react';
 
 class App extends React.Component {
-  render () {
-    return (
-      <h1>Hello Even Bigger Earth!</h1>
-    )
+  render() {
+    return <h1>Hello Even Bigger Earth!</h1>;
   }
 }
 
