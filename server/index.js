8ae0cce5de7df97059dfe1e44a6948f03ead2d16
Julian Bowman <julianfbowman@gmail.com>
2021-04-14 12:31:13 -0600

    Create skeleton for API connection

--- server/index.js
+++ server/index.js
@@ -1,18 +1,20 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
+const helper = require('../API/helper.js');
 
 app.use(express.static(__dirname + '/../client/dist'));
 
-app.get('https://app-hrsei-api.herokuapp.com/api/fec2/:hr-bld16/', (req, res) => {
-  res.send('Hello World!')
+app.get('/products', (req, res) => {
+  helper.apiRequest('/products', (error, data) => {
+    if (error) {
+      res.send(error);
+    } else {
+      res.send(data);
+    }
+  })
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
-
-
-
-
-
+  console.log(`Example app listening at http://localhost:${port}`);
+});
