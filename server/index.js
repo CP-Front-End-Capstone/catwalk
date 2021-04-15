d6783d525c2bf6b03ff52a44b9736fee24613db7
Tamir Amitai <tamir@Tamirs-MacBook-Air.local>
2021-04-15 07:53:52 -0600

    adds setting status code in app.get and adds server test file

--- server/index.js
+++ server/index.js
@@ -10,8 +10,10 @@ app.use(express.static(`${__dirname}/../client/dist`));
 app.get('/products', (req, res) => {
   helper.apiRequest('/products', (error, data) => {
     if (error) {
+      res.set(500);
       res.send(error);
     } else {
+      res.set(200);
       res.send(data);
     }
   });
