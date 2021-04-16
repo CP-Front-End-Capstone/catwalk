343313904600698b49a4381b3738254753cf9e79
Tamir Amitai <tamir@Tamirs-MacBook-Air.local>
2021-04-16 15:56:40 -0600

    final commit

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,7 +1,8 @@
 const React = require('react');
-const ReactDOM = require('react-dom');
-// const App = require('./App.jsx');
+const renderer = require('react-test-renderer');
+const { App } = require('./App.jsx');
 
 test('That App renders "Hello Even Bigger Earth!"', () => {
-    expect(document.getElementById('app').innerHTML).toBe('Hello Even Bigger Earth!');
-  });
+  const component = renderer.create(<App/>);
+  expect(text).toBe('Hello Even Bigger Earth!');
+});
