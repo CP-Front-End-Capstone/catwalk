aa01f75890c6292588e0b9936adcfb2cd39d0838
Jared Absher <jared.absher@gmail.com>
2021-04-19 14:28:58 -0400

    new change

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable react/no-unused-state */
 /* eslint-disable no-console */
 /* eslint-disable no-useless-constructor */
@@ -5,35 +6,8 @@
 import React from 'react';
 import axios from 'axios';
 
-<<<<<<< HEAD
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      products: [],
-    };
-  }
-
-  componentDidMount() {
-    axios.get('/products')
-      .then((res) => {
-        console.log(res.data);
-        this.setState({ products: res.data });
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }
-
-  render() {
-    return (
-      <h1>Hello Even Bigger Earth!</h1>
-    );
-  }
-=======
 function App(props) {
   return <h1>Hello Even Bigger Earth!</h1>;
->>>>>>> 0043b6eb71b50ecf08630b3a41e6a4d86fb8d630
 }
 
 export default App;
