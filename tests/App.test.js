8ecbf95cb871018324a52440dba5eaea92a7090a
Tamir Amitai <tamir@Tamirs-MacBook-Air.local>
2021-04-19 10:09:41 -0600

    formatting

--- tests/App.test.js
+++ tests/App.test.js
@@ -3,7 +3,6 @@ const Enzyme = require('enzyme');
 const config = require('./testconfig.js');
 const App = require('../client/src/App.jsx');
 
-
 const body = <App/>;
 
 console.log("INNERHTML:",body.innerHtml);
