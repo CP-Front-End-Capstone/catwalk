dd9b707b0f0616109446942d2233fce668fcfa07
Katie Brent <kab369@cornell.edu>
2021-04-19 12:09:16 -0600

    creates react context object

--- tests/App.test.jsx
+++ tests/App.test.jsx
@@ -1,8 +1,9 @@
+import App from '../client/src/App.jsx';
+
 const React = require('react');
 const Enzyme = require('enzyme');
 const config = require('./testconfig.js');
-const App = require('../client/src/App.jsx');
 
-const body = <App/>;
+const body = <App />;
 
-console.log("INNERHTML:",body.innerHtml);
+console.log("INNERHTML:", body);
