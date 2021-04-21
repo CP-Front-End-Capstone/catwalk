7e34281652ef2d4613d0ec2878592f17a4f86eed
Tamir Amitai <tamir@Tamirs-MacBook-Air.local>
2021-04-21 08:18:07 -0600

    refactors componentDidMount to make use of API helper function

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import axios from 'axios';
+import api from '../../API/helper';
 import productContext from './contexts/ProductContext';
 import ReviewsRatings from './widgets/reviews/ReviewsRatings.jsx';
 import QandA from './widgets/qa/QandA.jsx';
-import config from '../../API/config.js';
 
 class App extends React.Component {
   constructor(props) {
@@ -16,48 +15,67 @@ class App extends React.Component {
   }
 
   componentDidMount() {
-    axios({
-      url: 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-bld/products/18078',
-      method: 'GET',
-      headers: {
-        Authorization: config.TOKEN,
-      },
-    })
-      .then((res) => {
-        console.log(res);
+    api.fetchEndpoint('/products/18078', (error, product) => {
+      if (error) {
+        console.log('Error fetching product on component did mount', error);
+      } else {
+        console.log('PRODUCT', product);
         this.setState({
-          selectedProduct: res.data,
-          productId: res.data.id,
+          selectedProduct: product,
+          productId: product.id,
         });
-        axios({
-          url: 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-bld/products/18078/styles',
-          method: 'GET',
-          headers: {
-            Authorization: config.TOKEN,
-          },
-        })
-          .then((resp) => {
-            this.setState({ styles: resp.data.results });
-          })
-          .catch((err) => {
-            console.log('Error: ', err);
-          });
-      })
-      .catch((err) => {
-        console.log('Error: ', err);
-      });
+        api.fetchEndpoint('/products/18078/styles', (error, styles) => {
+          if (error) {
+            console.log('Error fetching style on component did mount', error);
+          } else {
+            console.log('STYLE', styles);
+            this.setState({ styles: styles.results });
+          }
+        });
+      }
+    });
+    // axios({
+    //   url: 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-bld/products/18078',
+    //   method: 'GET',
+    //   headers: {
+    //     Authorization: config.TOKEN,
+    //   },
+    // })
+    //   .then((res) => {
+    //     console.log(res);
+    //     this.setState({
+    //       selectedProduct: res.data,
+    //       productId: res.data.id,
+    //     });
+    //     axios({
+    //       url: 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-bld/products/18078/styles',
+    //       method: 'GET',
+    //       headers: {
+    //         Authorization: config.TOKEN,
+    //       },
+    //     })
+    //       .then((resp) => {
+    //         this.setState({ styles: resp.data.results });
+    //       })
+    //       .catch((err) => {
+    //         console.log('Error: ', err);
+    //       });
+    //   })
+    //   .catch((err) => {
+    //     console.log('Error: ', err);
+    //   });
   }
 
   render() {
+    const { currentProduct } = this.state.selectedProduct;
     return (
       <div>
-
         <h1>Hello Even Bigger Earth!</h1>
         <div>
-          <productContext.Provider value={this.state.selectedProduct}>
+          {/* <productContext.Provider value={1}>
             <ReviewsRatings />
             <QandA />
-          </productContext.Provider>
+          </productContext.Provider> */}
         </div>
       </div>
 
