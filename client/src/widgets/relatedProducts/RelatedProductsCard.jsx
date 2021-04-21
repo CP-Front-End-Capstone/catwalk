b75a953fd19aabe3b1fe89e9a14b59279b9b4a07
Julian Bowman <julianfbowman@gmail.com>
2021-04-21 16:40:00 -0600

    added divs to product card

--- client/src/widgets/relatedProducts/RelatedProductsCard.jsx
+++ client/src/widgets/relatedProducts/RelatedProductsCard.jsx
@@ -5,9 +5,15 @@ import React from 'react';
 function RelatedProductsCard({ product }) {
   return (
     <div>
-      <h5>{product.category}</h5>
-      <h4>{product.name}</h4>
-      <h5>{product.default_price}</h5>
+      <div>
+        <h5>{product.category}</h5>
+      </div>
+      <div>
+        <h6>{product.description}</h6>
+      </div>
+      <div>
+        <h5>{product.default_price}</h5>
+      </div>
     </div>
   );
 }
