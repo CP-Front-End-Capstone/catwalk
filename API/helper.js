dc30553ebeee2cfa6a74b1a36d51b2556aad94ea
Tamir Amitai <tamir@Tamirs-MacBook-Air.local>
2021-04-21 08:55:15 -0600

    starts refactoring of api helper to promises

--- API/helper.js
+++ API/helper.js
@@ -2,23 +2,18 @@
 const axios = require('axios');
 const config = require('./config.js');
 
-const fetchEndpoint = (endpoint, callback) => {
-  // const apiUrl = 'https://app-hrsei-api.herokuapp.com/api/fec2/:hr-bld';
-  // TODO - Use the axios module to request repos for a specific
-  // user from the github API
-  // The options object has been provided to help you out,
-  // but you'll have to fill in the URL
+const fetchEndpoint = (endpoint) => new Promise((resolve, reject) => {
   axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-bld${endpoint}`, {
     headers: {
       Authorization: config.TOKEN,
     },
   })
     .then((res) => {
-      callback(null, res.data);
+      resolve(res.data);
     })
     .catch((error) => {
-      callback(error, null);
+      reject(error);
     });
-};
+});
 
 module.exports.fetchEndpoint = fetchEndpoint;
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,86 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import api from '../../API/helper';
-import productContext from './contexts/ProductContext';
-import ReviewsRatings from './widgets/reviews/ReviewsRatings.jsx';
-import QandA from './widgets/qa/QandA.jsx';
+// import productContext from './contexts/ProductContext';
+// import ReviewsRatings from './widgets/reviews/ReviewsRatings.jsx';
+// import QandA from './widgets/qa/QandA.jsx';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      selectedProduct: { test: 'testing' },
-      productId: '',
-      styles: [],
-    };
-  }
+const App = () => {
+  const [productId, changeProductId] = useState('18078');
+  const [product, changeProduct] = useState();
+  const [styles, changeStyles] = useState();
 
-  componentDidMount() {
-    api.fetchEndpoint('/products/18078', (error, product) => {
-      if (error) {
-        console.log('Error fetching product on component did mount', error);
-      } else {
-        console.log('PRODUCT', product);
-        this.setState({
-          selectedProduct: product,
-          productId: product.id,
-        });
-        api.fetchEndpoint('/products/18078/styles', (error, styles) => {
-          if (error) {
-            console.log('Error fetching style on component did mount', error);
-          } else {
-            console.log('STYLE', styles);
-            this.setState({ styles: styles.results });
-          }
-        });
-      }
-    });
-    // axios({
-    //   url: 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-bld/products/18078',
-    //   method: 'GET',
-    //   headers: {
-    //     Authorization: config.TOKEN,
-    //   },
-    // })
-    //   .then((res) => {
-    //     console.log(res);
-    //     this.setState({
-    //       selectedProduct: res.data,
-    //       productId: res.data.id,
-    //     });
-    //     axios({
-    //       url: 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-bld/products/18078/styles',
-    //       method: 'GET',
-    //       headers: {
-    //         Authorization: config.TOKEN,
-    //       },
-    //     })
-    //       .then((resp) => {
-    //         this.setState({ styles: resp.data.results });
-    //       })
-    //       .catch((err) => {
-    //         console.log('Error: ', err);
-    //       });
-    //   })
-    //   .catch((err) => {
-    //     console.log('Error: ', err);
-    //   });
-  }
+  useEffect(() => {
+    api.fetchEndpoint(`/products/${productId}`)
+      .then((productData) => {
+        changeProduct(productData);
+        api.fetchEndpoint(`products`);
+      });
+  });
+  // changeStyles(stylesData.results);
 
-  render() {
-    const { currentProduct } = this.state.selectedProduct;
-    return (
+  return (
+    <div>
+      <h1>Hello Even Bigger Earth!</h1>
       <div>
-        <h1>Hello Even Bigger Earth!</h1>
-        <div>
-          {/* <productContext.Provider value={1}>
-            <ReviewsRatings />
-            <QandA />
-          </productContext.Provider> */}
-        </div>
+        {/* <productContext.Provider value={1}>
+          <ReviewsRatings />
+          <QandA />
+        </productContext.Provider> */}
+        {productId}
+        {JSON.stringify(product)}
+        {/* {JSON.stringify(styles)} */}
       </div>
-
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default App;
