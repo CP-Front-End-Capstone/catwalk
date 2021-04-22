854a1f94669658b34c78cdb193d156fc54e42395
Tamir Amitai <tamir@Tamirs-MacBook-Air.local>
2021-04-22 09:30:50 -0600

    ignore

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from 'react';
 import api from '../../API/helper';
 import productContext from './contexts/ProductContext';
 import QandA from './widgets/qa/QandA.jsx';
+import ReviewsRatings from './widgets/reviews/ReviewsRatings.jsx';
 
 const App = () => {
   const [productId, changeProductId] = useState('18078');
@@ -28,6 +29,7 @@ const App = () => {
       <h1>Hello Even Bigger Earth!</h1>
       <div>
         <productContext.Provider value={productId}>
+          <ReviewsRatings />
           <QandA changeContext={changeProductId} />
         </productContext.Provider>
       </div>
