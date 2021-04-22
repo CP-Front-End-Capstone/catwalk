8ca89e72c59a3470b4fdf23f3d5fd519ef9a76d9
Katie Brent <kab369@cornell.edu>
2021-04-22 16:30:40 -0600

    reviews breakdown rendering bar chart based on percentage. May need to refactor

--- client/src/widgets/reviews/ReviewBreakDown.jsx
+++ client/src/widgets/reviews/ReviewBreakDown.jsx
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react';
+import HSBar from 'react-horizontal-stacked-bar-chart';
 import reviewContext from '../../contexts/ReviewContext';
 
 const ReviewBreakDown = (props) => {
@@ -9,8 +10,17 @@ const ReviewBreakDown = (props) => {
 
   const recommendPercent = Number(reviewMeta.reviewsMeta.recommended.true) * 100
   / (totalRatings);
-  console.log(recommendPercent);
-  console.log(totalRatings);
+
+  const fiveStars = (reviewMeta.reviewsMeta.ratings[5])
+    ? (reviewMeta.reviewsMeta.ratings[5] * 100 / totalRatings) : 0;
+  const fourStars = (reviewMeta.reviewsMeta.ratings[4])
+    ? (reviewMeta.reviewsMeta.ratings[4] * 100 / totalRatings) : 0;
+  const threeStars = (reviewMeta.reviewsMeta.ratings[3])
+    ? (reviewMeta.reviewsMeta.ratings[3] * 100 / totalRatings) : 0;
+  const twoStars = (reviewMeta.reviewsMeta.ratings[2])
+    ? (reviewMeta.reviewsMeta.ratings[2] * 100 / totalRatings) : 0;
+  const oneStar = (reviewMeta.reviewsMeta.ratings[1])
+    ? (reviewMeta.reviewsMeta.ratings[1] * 100 / totalRatings) : 0;
 
   if (recommendPercent) {
     return (
@@ -20,12 +30,26 @@ const ReviewBreakDown = (props) => {
           {recommendPercent}
           % of reviews recommend this product
         </div>
-        <div>5 Stars</div>
-        <div>4 Stars</div>
-        <div>3 Stars</div>
-        <div>2 Stars</div>
-        <div>1 Stars</div>
-
+        <div>
+          5 Stars
+          <HSBar data={[{value: fiveStars, color: 'black' }, { value: 100 - fiveStars, color: 'grey' }]} />
+        </div>
+        <div>
+          4 Stars
+          <HSBar data={[{value: fourStars, color: 'black' }, { value: 100 - fourStars, color: 'grey' }]} />
+        </div>
+        <div>
+          3 Stars
+          <HSBar data={[{value: threeStars, color: 'black' }, { value: 100 - threeStars, color: 'grey' }]} />
+        </div>
+        <div>
+          2 Stars
+          <HSBar data={[{value: twoStars, color: 'black' }, { value: 100 - twoStars, color: 'grey' }]} />
+        </div>
+        <div>
+          1 Star
+          <HSBar data={[{value: oneStar, color: 'black' }, { value: 100 - oneStar, color: 'grey' }]} />
+        </div>
       </div>
     );
   }
