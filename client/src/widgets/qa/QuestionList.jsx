8ad8885c1ee86e17f02978139e3c5d0bdcd7cc06
Tamir Amitai <tamir@Tamirs-MacBook-Air.local>
2021-04-22 22:08:41 -0600

    commit

--- client/src/widgets/qa/QuestionList.jsx
+++ client/src/widgets/qa/QuestionList.jsx
@@ -3,13 +3,60 @@ import Question from './Question.jsx';
 import qaContext from '../../contexts/QaContext';
 
 const QuestionList = () => {
-  const { questions } = useContext(qaContext);
-  const list = questions.map((question) => (
-    <div className="col" key={question.question_id}>
-      <Question question={question} />
-    </div>
-  ));
-  return <>{list}</>;
+  const [count, changeCount] = useState([]);
+
+  const answers = [];
+  for (const id in question.answers) {
+    answers.push(question.answers[id]);
+  }
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    changeShowAll(true);
+  };
+
+  useEffect(() => {
+    if (!showAll) {
+      if (answers.length > 4) {
+        const temp = [];
+        for (let i = 0; i < 4; i++) {
+          temp.push(<Answer answer={answers[i]} key={answers[i].answer_id} />);
+        }
+        temp.push(<input
+          type="button"
+          className="btn"
+          key={question.question_id}
+          value="LOAD MORE ANSWERS"
+          onClick={handleClick}
+        />);
+        changeAnswerList(temp);
+      } else if (answers.length > 0) {
+        changeAnswerList(answers.map((answer) => (
+          <Answer answer={answer} key={answer.id} />
+        )));
+      }
+    } else {
+      changeAnswerList(answers.map((answer) => (
+        <Answer answer={answer} key={answer.id} />
+      )));
+    }
+  }, [showAll]);
+
+  return (
+    <>
+      <div className="col">
+        Q: {question.question_body}
+      </div>
+      {answersList}
+    </>
+  );
 };
+  // const { questions } = useContext(qaContext);
+  // const list = questions.map((question) => (
+  //   <div className="col" key={question.question_id}>
+  //     <Question question={question} />
+  //   </div>
+  // ));
+  // return <>{list}</>;
 
 export default QuestionList;
