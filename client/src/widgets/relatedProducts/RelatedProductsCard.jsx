168f5f2ca16deec77ce375dede887c2f2653618e
Julian Bowman <julianfbowman@gmail.com>
2021-04-24 11:04:21 -0600

    started adding buttons to cards

--- client/src/widgets/relatedProducts/RelatedProductsCard.jsx
+++ client/src/widgets/relatedProducts/RelatedProductsCard.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+/* eslint-disable jsx-a11y/control-has-associated-label */
 /* eslint-disable max-len */
 /* eslint-disable jsx-a11y/alt-text */
 /* eslint-disable no-console */
@@ -13,6 +15,7 @@ function RelatedProductsCard({ product, style }) {
     // if (style.results[0].photos[0].thumbnail_url)
     <div>
       <img className="card-img-top" src={style.results[0].photos[0].thumbnail_url ? style.results[0].photos[0].thumbnail_url : 'https://images.unsplash.com/photo-1519857609704-61e751edba25?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80'} width={300} height={400} />
+      <button type="button" className="btn btn-primary">Go somewhere</button>
       <div className="card-body">
         <h5 className="card-title">{product.category}</h5>
         <div className="card-text">
