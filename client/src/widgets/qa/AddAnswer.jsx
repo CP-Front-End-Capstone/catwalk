71c1ebbd1028b19cb7bdc36f3ca26a0ae17fddcc
Tamir Amitai <tamir@Tamirs-MacBook-Air.local>
2021-04-24 04:20:46 -0600

    maybe i fixed it

--- client/src/widgets/qa/AddAnswer.jsx
+++ client/src/widgets/qa/AddAnswer.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+<<<<<<< HEAD
 const AddAnswer = (props) => {
   const { question, name } = props;
   return (
@@ -53,11 +54,37 @@ const AddAnswer = (props) => {
             <div className="modal-footer">
               <button type="button" className="btn btn-primary">Submit</button>
             </div>
+=======
+const AddAnswer = () => (
+  <>
+    <span
+      className="btn h6 font-weight-light"
+      data-toggle="modal"
+      data-target="#addAnswerModal"
+    >
+      <u>Add Answer</u>
+    </span>
+    <div className="modal fade" id="addAnswerModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div className="modal-dialog">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h5 className="modal-title" id="exampleModalLabel">Add Answer</h5>
+            <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div className="modal-body">
+            ...
+          </div>
+          <div className="modal-footer">
+            <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+            <button type="button" className="btn btn-primary">Save changes</button>
+>>>>>>> parent of b874992... works on addAnswer modal
           </div>
         </div>
       </div>
-    </>
-  );
-};
+    </div>
+  </>
+);
 
 export default AddAnswer;
