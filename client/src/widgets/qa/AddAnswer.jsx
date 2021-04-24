862c186e51364ef5e4edc29be422a89337f5d2de
Tamir Amitai <tamir@Tamirs-MacBook-Air.local>
2021-04-24 06:14:18 -0600

    fixes modal needing unique id bug

--- client/src/widgets/qa/AddAnswer.jsx
+++ client/src/widgets/qa/AddAnswer.jsx
@@ -1,65 +1,39 @@
 import React from 'react';
 
-// const AddAnswer = () => (
-//   <>
-//     <span
-//       className="btn h6 font-weight-light"
-//       data-toggle="modal"
-//       data-target="#addAnswerModal"
-//     >
-//       <u>Add Answer</u>
-//     </span>
-//     <div className="modal fade" id="addAnswerModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-//       <div className="modal-dialog">
-//         <div className="modal-content">
-//           <div className="modal-header">
-//             <h5 className="modal-title" id="exampleModalLabel">Add Answer</h5>
-//             <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-//               <span aria-hidden="true">&times;</span>
-//             </button>
-//           </div>
-//           <div className="modal-body">
-//             ...
-//           </div>
-//           <div className="modal-footer">
-//             <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
-//             <button type="button" className="btn btn-primary">Save changes</button>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   </>
-// );
-
 const AddAnswer = (props) => {
-  const { question } = props;
+  const { question, name } = props;
+  console.log(question);
+  console.log(name);
   return (
     <>
       <span
         className="btn h6 font-weight-light"
         data-toggle="modal"
-        data-target="#addAnswerModal"
+        data-target={`#addAnswer${question.question_id}`}
       >
         <u>Add Answer</u>
       </span>
-      <div className="modal fade text-left" id="addAnswerModal" tabIndex="-1" aria-labelledby="addAnswerModalLabel" aria-hidden="true">
+      <div className="modal fade text-left" id={`addAnswer${question.question_id}`} tabIndex="-1" aria-labelledby="addAnswerModalLabel" aria-hidden="true">
         <div className="modal-dialog">
           <div className="modal-content">
             <div className="modal-header">
-              <h5 className="modal-title" id="addAnswerModalLabel">Add Answer</h5>
+              <h5 className="modal-title" id="addAnswerModalLabel">
+                Submit your answer:
+              </h5>
               <button type="button" className="close" data-dismiss="modal" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
               </button>
             </div>
             <div className="modal-body">
+              <div>{name}:&nbsp;{question.question_body}</div>
             <form>
               <label>
-                *Answer:
+                *Your Answer:
                 <textarea name="answer" maxLength="1000" />
               </label>
               <br />
               <label>
-                *Name:
+                *Your Name:
                 <input type="text" name="name" maxLength="60" />
                 <br />
                 (For privacy reasons, do not use your full name or email address)
@@ -67,7 +41,7 @@ const AddAnswer = (props) => {
               </label>
               <br />
               <label>
-                *Email:
+                *Your Email:
                 <input type="email" name="email" maxLength="60" />
                 <br />
                 (For authentication reasons, you will not be emailed)
