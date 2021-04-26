ce45f9643ed6c5dcf4b6d231d695dd7b8f089d55
Julian Bowman <julianfbowman@gmail.com>
2021-04-26 15:13:39 -0600

    fixed add to outfit funtionality

--- client/src/widgets/relatedProducts/MyOutfitList.jsx
+++ client/src/widgets/relatedProducts/MyOutfitList.jsx
@@ -1,9 +1,11 @@
+/* eslint-disable max-len */
+/* eslint-disable no-lone-blocks */
 /* eslint-disable no-undef */
 /* eslint-disable no-console */
 /* eslint-disable react/prop-types */
 /* eslint-disable import/extensions */
 import React, { useState } from 'react';
-import Carousel, { consts } from 'react-elastic-carousel';
+// import Carousel, { consts } from 'react-elastic-carousel';
 import MyOutfitCard from './MyOutfitCard.jsx';
 import ProductCard from './ProductCard.jsx';
 
@@ -12,6 +14,27 @@ function MyOutfitList({ currentProduct, styles }) {
 
   return (
     <div className="container-fluid">
+      <div className="row">
+        <div className="col-sm-3">
+          <div className="card-group">
+            <div className="card">
+              <MyOutfitCard updateFit={setOutfit} />
+            </div>
+          </div>
+        </div>
+        <div className="col-sm-3">
+          <div className="card-group">
+            {outfit ? <ProductCard currentProduct={currentProduct} style={styles[0]} updateFit={setOutfit} /> : null}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default MyOutfitList;
+
+{ /* <div className="container-fluid">
       <div className="row">
         <Carousel itemPosition={consts.START} itemsToShow={1}>
           <div className="col-sm-3">
@@ -30,8 +53,4 @@ function MyOutfitList({ currentProduct, styles }) {
           </div>
         </Carousel>
       </div>
-    </div>
-  );
-}
-
-export default MyOutfitList;
+    </div>  */ }
