a8b0d300e694eaedabf6689897745bd615f007a7
Tamir Amitai <tamir@Tamirs-MacBook-Air.local>
2021-04-27 10:23:26 -0600

    works on test

--- tests/QuestionList.test.jsx
+++ tests/QuestionList.test.jsx
@@ -5,7 +5,7 @@ import QuestionList from '../client/src/widgets/qa/QuestionList.jsx';
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<QuestionList />, div);
-  console.log(div.getElementbyId(''))
+  console.log(div.getElementbyId('questionlist'));
   // expect().to.be('h-75 overflow-auto');
   ReactDOM.unmountComponentAtNode(div);
 });
