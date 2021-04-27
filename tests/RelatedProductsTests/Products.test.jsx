b2dc1bbacad537588428787622739fa13e9d054d
Julian Bowman <julianfbowman@gmail.com>
2021-04-27 16:51:06 -0600

    deleted carousel test files

--- tests/RelatedProductsTests/Products.test.jsx
+++ tests/RelatedProductsTests/Products.test.jsx
@@ -2,14 +2,14 @@
 /* eslint-disable import/extensions */
 /* eslint-disable no-undef */
 /* eslint-disable no-console */
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import Enzyme, { shallow } from 'enzyme';
-// import RelatedProducts from '../../client/src/widgets/relatedProducts/Products.jsx';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Enzyme, { shallow } from 'enzyme';
+import RelatedProducts from '../../client/src/widgets/relatedProducts/Products.jsx';
 
-// describe('Product Component', () => {
-//   it('renders without crashing', () => {
-//     const wrapper = shallow(<RelatedProducts />);
-//     expect(wrapper.find('#products')).to.be(true);
-//   });
-// });
+describe('Product Component', () => {
+  it('renders without crashing', () => {
+    const wrapper = shallow(<RelatedProducts />);
+    expect(wrapper.find('#products')).to.be(true);
+  });
+});
