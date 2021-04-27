24f3dc3388d48295d43def75c41e06d3822105c3
Tamir Amitai <tamir@Tamirs-MacBook-Air.local>
2021-04-27 15:40:01 -0600

    removes Modal.setAppElement since breaks tests

--- tests/QuestionList.test.jsx
+++ tests/QuestionList.test.jsx
@@ -1,11 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Enzyme from 'enzyme';
+import config from './testconfig.js';
 import QuestionList from '../client/src/widgets/qa/QuestionList.jsx';
+import qaContext from '../client/src/contexts/QaContext.js';
+import questionsData from './questionsData.js';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<QuestionList />, div);
-  console.log(div.getElementbyId('questionlist'));
-  // expect().to.be('h-75 overflow-auto');
-  ReactDOM.unmountComponentAtNode(div);
+  const productId = questionsData.product_id;
+  const productName = 'product name';
+  const questions = questionsData.results;
+  const changeQuestions = () => {};
+  const count = 4;
+  const changeCount = () => {};
+
+  const wrapper = Enzyme.mount(
+    <qaContext.Provider value={{
+      questions,
+      changeQuestions,
+      count,
+      changeCount,
+      productName,
+      productId,
+    }}
+    >
+      <QuestionList />
+    </qaContext.Provider>,
+  );
+  console.log(wrapper);
 });
