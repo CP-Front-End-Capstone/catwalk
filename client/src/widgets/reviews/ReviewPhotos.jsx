9f6a3c319bc35c905eae756fdbd56ee742236748
Katie Brent <kab369@cornell.edu>
2021-04-27 13:24:04 -0600

    fixes image modal bug!!!

--- client/src/widgets/reviews/ReviewPhotos.jsx
+++ client/src/widgets/reviews/ReviewPhotos.jsx
@@ -5,18 +5,20 @@ const ReviewPhotos = (props) => (
     const photoId = photo.id;
     const photoUrl = photo.url;
 
+    console.log(photoUrl);
+
     return (
 
       <div key={photoId}>
-        <img alt={photoId} src={photoUrl} width="50px" length="50px" data-toggle="modal" data-target="#imageModal" />
-        <div className="modal" id="imageModal" tabIndex="-1" role="dialog" aria-hidden="true">
+        <img alt={photoId} src={photoUrl} width="50px" length="50px" data-toggle="modal" data-target={`#modal${photoId}`} />
+        <div className="modal" id={`modal${photoId}`} tabIndex="-1" role="dialog" aria-hidden="true">
           <div className="modal-dialog" role="document">
             <div className="modal-content">
               <div className="modal-body">
                 <button type="button" className="close" data-dismiss="modal" aria-label="Close">
                   <span aria-hidden="true">&times;</span>
                 </button>
-                <img alt={photoId} src={photoUrl} width="470px" length="470px" data-toggle="modal" data-target="#imageModal" />
+                <img alt={photoId} src={photoUrl} width="470px" length="470px" />
               </div>
             </div>
           </div>
